937b7b8fe0258e3cb407b186f20c749cfc65bf5d
Karina Miranda <Karina.Miranda@frt.utn.edu.ar>
2026-06-27 21:43:32 -0300

    se instalo Morgan

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express()
 const PORT = 3001
 
 app.use(cors())
+app.use(express.json())
 app.listen(PORT, () => {
     console.info(`servidor activo en el puerto ${PORT}`);
+});
+app.get("/api/tarea", (req, res)=> {
+console.log('Me llego solicitud nueva');
+res.json ( 
+    { 
+    mensaje:"conexion creada",
+    saludo:"Bienvenido a nuestro backend",
+}
+)
 });
